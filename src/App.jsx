e14b118384902798a6520eec419422197ea1f181
amarnathbhat <bhatamar8@gmail.com>
2024-01-10 22:57:15 +0530

    Footer Added

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,21 @@ import Categories from "./components/Categories/Categories.jsx";
 import Courses from "./components/Courses/Courses.jsx";
 import Banner from "./components/Banner/Banner.jsx";
 import Stats from "./components/Stats/Stats.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className="bg-black">
+      <div className="bg-black abcd">
         <Navbar />
         <Hero />
         <Categories />
         <Courses />
         {/* <Banner /> */}
         <Stats />
+        <Footer />
       </div>
     </>
   );
